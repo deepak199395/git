20cfff40d6ac0ba7641abea6f559f87fb00a1a68
Deepak Yadav <deepak@Deepaks-MacBook-Pro.local>
2024-11-15 12:44:41 +0530

    js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ function submitForm() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
-
+// hello world
     if (name && email && message) {
         document.getElementById("responseMessage").textContent = "Thank you for your message!";
         document.getElementById("contactForm").reset();
     } else {
         document.getElementById("responseMessage").textContent = "Please fill out all fields.";
-        
+
     }
 }
+// no way
